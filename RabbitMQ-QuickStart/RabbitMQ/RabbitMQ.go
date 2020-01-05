8737077f867182e157257eda1a17b230a53520d1
birdream <bird.dream.norman@gmail.com>
2020-01-05 22:57:21 +0800

    RabbitMQ instance init

---
+++ RabbitMQ-QuickStart/RabbitMQ/RabbitMQ.go
@@ -0,0 +1,140 @@
+package RabbitMQ
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+const MQURL = "ampq://guest:guest@127.0.0.1:5672/imooc"
+
+type RabbitMQ struct {
+	conn      *amqp.Connection
+	channel   *amqp.Channel
+	QueueName string
+	Exchange  string
+	Key       string
+	Mqurl     string
+}
+
+func NewRabbitMQ(queueName, exchange, key string) *RabbitMQ {
+	return &RabbitMQ{
+		QueueName: queueName,
+		Exchange:  exchange,
+		Key:       key,
+		Mqurl:     MQURL,
+	}
+}
+
+func (r *RabbitMQ) Destory() {
+	r.channel.Close()
+	r.conn.Close()
+}
+
+func (r *RabbitMQ) failOnErr(err error, message string) {
+	if err != nil {
+		log.Fatalf("%s:%s", message, err)
+		panic(fmt.Sprintf("%s:%s", message, err))
+	}
+}
+
+func NewRabbitSimple(queueName string) *RabbitMQ {
+	rbmq := NewRabbitMQ(queueName, "", "")
+	var err error
+
+	rbmq.conn, err = amqp.Dial(rbmq.Mqurl)
+	rbmq.failOnErr(err, "fail to connect rabbit itmq!")
+
+	rbmq.channel, err = rbmq.conn.Channel()
+	rbmq.failOnErr(err, "fail to open a channel")
+
+	return rbmq
+}
+
+//直接模式队列生产
+func (r *RabbitMQ) PublishSimple(message string) {
+	//1.申请队列，如果队列不存在会自动创建，存在则跳过创建
+	_, err := r.channel.QueueDeclare(
+		r.QueueName,
+		//是否持久化
+		false,
+		//是否自动删除
+		false,
+		//是否具有排他性
+		false,
+		//是否阻塞处理
+		false,
+		//额外的属性
+		nil,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//调用channel 发送消息到队列中
+	r.channel.Publish(
+		r.Exchange,
+		r.QueueName,
+		//如果为true，根据自身exchange类型和routekey规则无法找到符合条件的队列会把消息返还给发送者
+		false,
+		//如果为true，当exchange发送消息到队列后发现队列上没有消费者，则会把消息返还给发送者
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+}
+
+//simple 模式下消费者
+func (r *RabbitMQ) ConsumeSimple() {
+	//1.申请队列，如果队列不存在会自动创建，存在则跳过创建
+	q, err := r.channel.QueueDeclare(
+		r.QueueName,
+		//是否持久化
+		false,
+		//是否自动删除
+		false,
+		//是否具有排他性
+		false,
+		//是否阻塞处理
+		false,
+		//额外的属性
+		nil,
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//接收消息
+	msgs, err := r.channel.Consume(
+		q.Name, // queue
+		//用来区分多个消费者
+		"", // consumer
+		//是否自动应答
+		true, // auto-ack
+		//是否独有
+		false, // exclusive
+		//设置为true，表示 不能将同一个Conenction中生产者发送的消息传递给这个Connection中 的消费者
+		false, // no-local
+		//列是否阻塞
+		false, // no-wait
+		nil,   // args
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	forever := make(chan bool)
+	//启用协程处理消息
+	go func() {
+		for d := range msgs {
+			//消息逻辑处理，可以自行设计逻辑
+			log.Printf("Received a message: %s", d.Body)
+
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+
+}
