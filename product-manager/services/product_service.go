8a8d2f64c2f484b64f4f4bf9cd61d103fb2970ec
birdream <bird.dream.norman@gmail.com>
2020-01-07 17:05:28 +0800

    services

--- product-manager/services/product_service.go
+++ product-manager/services/product_service.go
@@ -5,6 +5,7 @@ import (
 	"product-manager/repositories"
 )
 
+// IProductService interface
 type IProductService interface {
 	GetProductByID(int64) (*datamodels.Product, error)
 	GetAllProduct() ([]*datamodels.Product, error)
@@ -13,6 +14,37 @@ type IProductService interface {
 	UpdateProduct(product *datamodels.Product) error
 }
 
+// ProductService real struct
 type ProductService struct {
 	productRepository repositories.IProduct
+}
+
+// NewProductService init method
+func NewProductService(repository repositories.IProduct) IProductService {
+	return &ProductService{repository}
+}
+
+// GetProductByID ..
+func (p *ProductService) GetProductByID(productID int64) (*datamodels.Product, error) {
+	return p.productRepository.SelectByKey(productID)
+}
+
+// GetAllProduct ..
+func (p *ProductService) GetAllProduct() ([]*datamodels.Product, error) {
+	return p.productRepository.SelectAll()
+}
+
+// DeleteProductByID ..
+func (p *ProductService) DeleteProductByID(productID int64) bool {
+	return p.productRepository.Delete(productID)
+}
+
+// InsertProduct ..
+func (p *ProductService) InsertProduct(product *datamodels.Product) (int64, error) {
+	return p.productRepository.Insert(product)
+}
+
+// UpdateProduct ..
+func (p *ProductService) UpdateProduct(product *datamodels.Product) error {
+	return p.productRepository.Update(product)
 }
