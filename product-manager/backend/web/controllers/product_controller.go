22dd2038438a860d2440bad05b2ca6221d551ed6
birdream <bird.dream.norman@gmail.com>
2020-01-07 17:45:18 +0800

    PostUpdate method;

--- product-manager/backend/web/controllers/product_controller.go
+++ product-manager/backend/web/controllers/product_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"product-manager/common"
+	"product-manager/datamodels"
 	"product-manager/services"
 
 	"github.com/kataras/iris"
@@ -22,4 +24,22 @@ func (p *ProductController) GetAll() mvc.View {
 			"productArray": productArray,
 		},
 	}
+}
+
+// PostUpdate 修改商品
+func (p *ProductController) PostUpdate() {
+	product := &datamodels.Product{}
+	p.Ctx.Request().ParseForm()
+
+	dec := common.NewDecoder(&common.DecoderOptions{TagName: "imooc"})
+	if err := dec.Decode(p.Ctx.Request().Form, product); err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	err := p.ProductService.UpdateProduct(product)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	p.Ctx.Redirect("/product/all")
 }
