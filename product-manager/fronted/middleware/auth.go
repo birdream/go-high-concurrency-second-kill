c7142af7078fab0043bced176d4017e38db87c50
birdream <bird.dream.norman@gmail.com>
2020-03-02 22:41:46 +0800

    stash

--- product-manager/fronted/middleware/auth.go
+++ product-manager/fronted/middleware/auth.go
@@ -4,11 +4,11 @@ import "github.com/kataras/iris"
 
 func AuthConProduct(ctx iris.Context) {
 	uid := ctx.GetCookie("uid")
-	if uid =="" {
+	if uid == "" {
 		ctx.Application().Logger().Debug("必须先登录!")
 		ctx.Redirect("/user/login")
 		return
 	}
-	ctx.Application().Logger().Debug("已经登陆")
+	ctx.Application().Logger().Debug("已经登陆1")
 	ctx.Next()
 }
